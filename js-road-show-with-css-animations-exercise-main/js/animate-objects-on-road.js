302db9361c44163bf32438ce851e22c4eb743f6b
eaa25mtha <eaa25mtha@students.eaaa.dk>
2025-11-05 11:09:49 +0100

    road animationer

---
+++ js-road-show-with-css-animations-exercise-main/js/animate-objects-on-road.js
@@ -0,0 +1,71 @@
+"use strict";
+
+
+/*-----------------------------------------------
+DOM elementer
+-------------------------------------------------*/ 
+//hent DOM elementer med document.getElementById og document.querySelector
+
+const getRedCar = document.getElementById("redCar");  //tager fat i bil elementet rød bil, med id "redCar"
+const getPoliceCar = document.getElementById("policeCar");
+const getBlueCar = document.getElementById("blueCar");
+
+const getSun = document.querySelector(".sun");  //tager fat i sol elementet med class "sun" derfor sætter vi også "." på
+const getScene = document.querySelector(".scene");
+
+
+
+/*-----------------------------------------------
+Audio objekter
+-------------------------------------------------*/ 
+
+const soundRedCar = new Audio();
+soundRedCar.src = "sound/red-car-horn.wav";
+
+const soundPoliceCar = new Audio();
+soundPoliceCar.src = "sound/police-car-sound.wav";
+
+const soundBlueCar = new Audio();
+soundBlueCar.src = "sound/blue-car-sound.wav";
+
+
+/*---------------------------------------------
+addEventListeners på DOM elementerne
+-----------------------------------------------*/
+
+//den røde bil
+if (getRedCar) {
+    getRedCar.addEventListener("click", function() {
+        soundRedCar.play();
+    });
+}
+
+//politi bilen 
+if (getPoliceCar) {
+    getPoliceCar.addEventListener("click", function() {
+        soundPoliceCar.play();
+    });
+}
+
+//den blå bil 
+if (getBlueCar) {
+    getBlueCar.addEventListener("click", function() {
+        soundBlueCar.play();
+    });
+}
+
+//skift fra nat til dag med click 
+// && syntaksen ligesom at == betyder lig med 
+if (getSun && getScene) {
+    getSun.addEventListener("click", function() {
+        getScene.classList.toggle("night");
+    });
+}
+
+
+
+
+
+
+
+
