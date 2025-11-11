a04086958f1af157e1d5e72fd16a08608c8bee3d
eaa25mtha <eaa25mtha@students.eaaa.dk>
2025-11-11 19:28:41 +0100

    fejlrettelser

--- js-road-show-with-css-animations-exercise-main/js/animate-objects-on-road.js
+++ js-road-show-with-css-animations-exercise-main/js/animate-objects-on-road.js
@@ -1,8 +1,96 @@
 "use strict";
 
+/*------------------------------------------
+DATA OBJEKTER I ARRAY - OBYGGER DATASTRUKTUR
+--------------------------------------------*/
+const carInfo = [
+    {
+        className: "car1", 
+        carBrand: "Ford", 
+        carModel: "Mustang", 
+        releaseYear: 1974, 
+        color: "red", 
+        fuelType: "Diesel"
+    },
 
-/*-----------------------------------------------
-DOM elementer
+    {
+        className: "car2", 
+        carBrand: "Skoda", 
+        carModel: "Fabia", 
+        releaseYear: 2020,
+        color: "Grey", 
+        fuelType: "Diesel"
+    }, 
+
+    {
+        className: "car3", 
+        carBrand: "Hyundai", 
+        carModel: "Turbo", 
+        releaseYear: 2016, 
+        color: "Blue", 
+        fuelType: "Benzin"
+    },
+
+    {
+        className: "car4",
+        carBrand: "Volvo",
+        carModel: "Nimbus",
+        releaseYear: 2003,
+        color: "Yellow",
+        fuelType: "Benzin"
+    }
+]
+
+
+/*--------------------------------------------------
+DOM CONTENT LOAD FUNKTION OG FOR EACH LOOP MED INFO
+----------------------------------------------------*/
+
+//DOMContentLoaded, venter med at kører JS, indtil hele html-siden er indlæst
+//hele koden er inde i denne funktion
+document.addEventListener("DOMContentLoaded", () => {
+
+//finder tooltip id og gemmer det i en variabel
+const tooltip = document.getElementById("tooltip");
+
+
+//funktion der viser tooltip boksen med biloplysninger
+//parmeter: html = den tekst indeholdende html-tags som vi vil vise i tooltip'en
+function showTooltip (html) {
+    if (tooltip) {
+        //indsætter teksten i tooltip'en
+        tooltip.innerHTML = html; 
+
+        //gør tooltip'en synlig med css class
+        tooltip.classList.add("is-visible");  
+        
+        setTimeout(function() {
+            tooltip.classList.remove("is-visible");
+        },5000);  //,5000 fortæller hvor lang tid boksen skal være synlig, 
+    }
+}
+ 
+carInfo.forEach((car) => {     //tager fat i alt der har med "car" at gøre i html'en og loop'er det
+    document.querySelectorAll("." + car.className).forEach((elem) => {
+        elem.addEventListener("mouseover", () => {
+            const carDetails = `
+            <strong> ${car.carBrand}</strong> <br>
+            Model: ${car.carModel}<br>
+            Release year: ${car.releaseYear}<br>
+            Color: ${car.color}<br>
+            Fuel type: ${car.fuelType}
+            `;
+
+            showTooltip(carDetails); //funktionen showTooltip bliver kaldt, med variablen carDetails (med html) i
+        });
+
+    });
+});
+
+
+
+/*------------------------------------------------
+DOM ELEMENTER, VARIABLER
 -------------------------------------------------*/ 
 //hent DOM elementer med document.getElementById og document.querySelector
 
@@ -10,14 +98,12 @@ const getRedCar = document.getElementById("redCar");  //tager fat i bil elemente
 const getPoliceCar = document.getElementById("policeCar");
 const getBlueCar = document.getElementById("blueCar");
 const getTruck = document.getElementById("truck");
-
 const getSun = document.querySelector(".sun");  //tager fat i sol elementet med class "sun" derfor sætter vi også "." på
 const getScene = document.querySelector(".scene");
 
 
-
 /*-----------------------------------------------
-Audio objekter
+AUDIO OBJEKTER
 -------------------------------------------------*/ 
 
 const soundRedCar = new Audio();
@@ -28,6 +114,7 @@ soundPoliceCar.src = "sound/police-car-sound.wav";
 
 const soundBlueCar = new Audio();
 soundBlueCar.src = "sound/blue-car-sound.wav";
+
 
 
 /*---------------------------------------------
@@ -62,6 +149,17 @@ if (getSun && getScene) {
         getScene.classList.toggle("night");
     });
 }
+
+
+}); //slut fra DOMContentLoaded funktionen
+
+
+
+
+
+
+
+
 
 
 
